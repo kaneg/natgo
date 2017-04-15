ec00a53fbe05c19efd3cee885e1ded384e5777b4
Kane <gzkaneg@gmail.com>
2017-04-15 18:55:13 +0800

    Use log instead of fmt to print out log

--- natgo-client.go
+++ natgo-client.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-    "fmt"
     "net"
     "./natgo"
     "sync"
@@ -9,6 +8,7 @@ import (
     "time"
     "strings"
     "errors"
+    "log"
 )
 
 var serviceMap = make(map[string]string)
@@ -17,9 +17,9 @@ var mgrChannelLock = sync.Mutex{}
 var isServerAlive = true
 
 func main() {
-    fmt.Println("Start NAT client")
+    log.Println("Start NAT client")
     if (len(os.Args) < 3) {
-        fmt.Println("Usage: natgo-client <remoteHost:port>  <servicePort:targetHost:port> [servicePort:targetHost:port] ")
+        log.Println("Usage: natgo-client <remoteHost:port>  <servicePort:targetHost:port> [servicePort:targetHost:port] ")
         return
     }
     remoteAddr := os.Args[1]
@@ -34,15 +34,15 @@ func main() {
         services += ","
     }
     services = strings.Trim(services, ",")
-    fmt.Println(services)
+    log.Println(services)
 
-    fmt.Println("Connect to control address  ", remoteAddr)
+    log.Println("Connect to control address  ", remoteAddr)
     wg := sync.WaitGroup{}
     wg.Add(1)
     for {
         connRemote, err := net.DialTimeout("tcp", remoteAddr, 10 * time.Second)
         if err != nil {
-            fmt.Println("Can't connect to remote: ", err)
+            log.Println("Can't connect to remote: ", err)
             time.Sleep(10 * time.Second)
             continue
         }
@@ -51,7 +51,7 @@ func main() {
 
         err = natgo.ClientRegisterRequest(connRemote, services)
         if (err != nil) {
-            fmt.Println("Can not register to server.")
+            log.Println("Can not register to server.")
             continue
         }
         connRemote.SetReadDeadline(time.Time{})
@@ -64,7 +64,7 @@ func main() {
                 targetAddr := serviceMap[service]
                 work(remoteAddr, targetAddr, sessionId)
             } else {
-                fmt.Println("Failed to start session with server.")
+                log.Println("Failed to start session with server.")
                 connRemote.Close()
                 break
             }
@@ -77,13 +77,13 @@ func main() {
 func heartBeat(conn net.Conn) {
     for {
         time.Sleep(30 * time.Second)
-        fmt.Println("Begin heart beat")
-        fmt.Println("Get mgr conn lock")
+        log.Println("Begin heart beat")
+        log.Println("Get mgr conn lock")
         mgrChannelLock.Lock()
         isServerAlive = false
         _, err := conn.Write([]byte{natgo.CMD_HEART_BEAT_REQUEST})
         if (err != nil) {
-            fmt.Println("Failed to send heartbeat request to server, close the connection.", err)
+            log.Println("Failed to send heartbeat request to server, close the connection.", err)
             conn.Close()
             mgrChannelLock.Unlock()
             return
@@ -92,36 +92,36 @@ func heartBeat(conn net.Conn) {
         time.Sleep(2 * time.Second) //waiting for response
 
         if (isServerAlive) {
-            fmt.Println("Get heartbeat response from server")
+            log.Println("Get heartbeat response from server")
         } else {
-            fmt.Println("Failed to get heartbeat response, close the connection.", err)
+            log.Println("Failed to get heartbeat response, close the connection.", err)
             conn.Close()
             mgrChannelLock.Unlock()
             return
         }
-        fmt.Println("Release mgr lock")
+        log.Println("Release mgr lock")
         mgrChannelLock.Unlock()
-        fmt.Println("Done heartbeat")
+        log.Println("Done heartbeat")
     }
 }
 
 func work(remoteAddr, targetAddr string, sessionId int32) {
-    fmt.Println("Connecting target host ", targetAddr)
+    log.Println("Connecting target host ", targetAddr)
     targetConn := connectPort(targetAddr)
     if targetConn == nil {
-        fmt.Println("Failed to connect to target addr")
+        log.Println("Failed to connect to target addr")
         return
     }
 
-    fmt.Println("Connecting to session remoteAddr ", remoteAddr)
+    log.Println("Connecting to session remoteAddr ", remoteAddr)
     sessionConn := connectPort(remoteAddr)
     if (sessionConn == nil) {
-        fmt.Println("Failed to connect to remote addr")
+        log.Println("Failed to connect to remote addr")
         return
     }
     natgo.ClientReplySessionRequest(sessionConn, sessionId)
 
-    fmt.Println("Begin transfer data ...")
+    log.Println("Begin transfer data ...")
 
     natgo.ConnectionExchange(targetConn, sessionConn)
 }
@@ -129,47 +129,47 @@ func work(remoteAddr, targetAddr string, sessionId int32) {
 func connectPort(remoteAddr string) net.Conn {
     conn, err := net.DialTimeout("tcp", remoteAddr, 5 * time.Second)
     if (err != nil) {
-        fmt.Println("Can't connect to addr: ", err)
+        log.Println("Can't connect to addr: ", err)
         return nil
     }
-    fmt.Println("Connectted:", conn.RemoteAddr())
+    log.Println("Connectted:", conn.RemoteAddr())
     return conn
 }
 
 func serverStartSessionResponse(conn net.Conn) (int32, string, error) {
-    fmt.Println("ServerStartSessionResponse")
+    log.Println("ServerStartSessionResponse")
     for {
         request := make([]byte, 20)
-        fmt.Println("Begin read cmd...")
+        log.Println("Begin read cmd...")
         _, err := conn.Read(request)
         if (err != nil) {
             return 0, "", err
         }
-        fmt.Println("Read cmd:", request)
+        log.Println("Read cmd:", request)
         if (request[0] == natgo.CMD_HEART_BEAT_RESPONSE) {
-            fmt.Println("Get heart beat cmd.")
+            log.Println("Get heart beat cmd.")
             isServerAlive = true
             continue
         } else if (request[0] != natgo.CMD_SERVER_START_SESSION_REQUEST) {
-            fmt.Println("Invalid cmd")
+            log.Println("Invalid cmd")
             return 0, "", errors.New("Invalid cmd from server")
         }
         requestId := natgo.BytesToInt32(request[1:5])
-        fmt.Println("request id:", requestId)
+        log.Println("request id:", requestId)
 
         serviceBuffer := request[5:]
 
         service := string(serviceBuffer)
         service = strings.Trim(service, "\x00")
-        fmt.Println("Client got service:", service)
+        log.Println("Client got service:", service)
 
         response := make([]byte, 1)
         response[0] = natgo.CMD_SERVER_START_SESSION_RESPONSE
-        fmt.Println("Sending response ", response)
+        log.Println("Sending response ", response)
         mgrChannelLock.Lock()
         conn.Write(response)
         mgrChannelLock.Unlock()
-        fmt.Println("Sent CMD_SERVER_START_SESSION_RESPONSE response")
+        log.Println("Sent CMD_SERVER_START_SESSION_RESPONSE response")
         return requestId, service, nil
     }
 }
--- natgo-server.go
+++ natgo-server.go
@@ -3,12 +3,12 @@ package main
 import (
     "net"
     "sync"
-    "fmt"
     "./natgo"
     "os"
     "strings"
     "time"
     "math/rand"
+    "log"
 )
 
 var natConnectionPool = make(map[string]net.Conn)
@@ -17,9 +17,9 @@ var waitingGuestConnectionPool = make(map[int32]net.Conn)
 var mgrChannelLock = sync.Mutex{}
 
 func main() {
-    fmt.Println("Start NAT server")
+    log.Println("Start NAT server")
     if (len(os.Args) < 3) {
-        fmt.Println("Usage: natgo-server <managerPort>  <servicePort> [servicePort] ...")
+        log.Println("Usage: natgo-server <managerPort>  <servicePort> [servicePort] ...")
         return
     }
 
@@ -30,43 +30,43 @@ func main() {
     wg.Wait()
 }
 func listenForNAT(localPort string) {
-    fmt.Println("Start NAT thread on port " + localPort)
+    log.Println("Start NAT thread on port " + localPort)
     ln, err := net.Listen("tcp", ":" + localPort)
     if err != nil {
-        fmt.Println(err)
+        log.Println(err)
         panic(err)
     }
-    fmt.Println("Listen on port:" + localPort, ln.Addr())
+    log.Println("Listen on port:" + localPort, ln.Addr())
 
     for {
         conn, err := ln.Accept()
         if err != nil {
-            fmt.Println(err)
+            log.Println(err)
             conn.Close()
         } else {
-            fmt.Println("Got a new connection from nat:", conn.RemoteAddr())
-            handleClient(conn)
+            log.Println("Got a new connection from nat:", conn.RemoteAddr())
+            go handleClient(conn)
         }
     }
 }
 
 func handleClient(conn net.Conn) error {
-    fmt.Println("handleClient")
+    log.Println("handleClient")
     request := make([]byte, 1)
     conn.Read(request)
 
     cmd := request[0]
     if (cmd == natgo.CMD_REGISTER_CLIENT_REQUEST) {
-        fmt.Println("Get register request from client.")
+        log.Println("Get register request from client.")
         portBuffer := make([]byte, 1024)
         conn.Read(portBuffer)
         portStr := string(portBuffer)
         portStr = strings.Trim(portStr, "\x00")
-        fmt.Println("Port service from client:", portStr)
+        log.Println("Port service from client:", portStr)
         response := make([]byte, 1)
         response[0] = natgo.CMD_REGISTER_CLIENT_RESPONSE
         conn.Write(response)
-        fmt.Println("Send client register response")
+        log.Println("Send client register response")
         services := strings.Split(portStr, ",")
         for _, v := range services {
             oldConn := natConnectionPool[v]
@@ -76,21 +76,20 @@ func handleClient(conn net.Conn) error {
             }
             natConnectionPool[v] = conn
         }
-        //go heartbeat(conn)
         go processMgrChannel(conn)
     } else if (cmd == natgo.CMD_CLIENT_REPLY_SESSION_REQUEST) {
-        fmt.Println("Got a session connection from client:", conn)
+        log.Println("Got a session connection from client:", conn)
         request = make([]byte, 4)
         conn.Read(request)
         sessionId := natgo.BytesToInt32(request)
-        fmt.Println("Get session id from client:", sessionId)
+        log.Println("Get session id from client:", sessionId)
         response := make([]byte, 1)
         response[0] = natgo.CMD_CLIENT_REPLY_SESSION_RESPONSE
         conn.Write(response)
-        fmt.Println("Send client reply session response")
+        log.Println("Send client reply session response")
 
         guestConn := waitingGuestConnectionPool[sessionId]
-        fmt.Println("Get guest connection from pool:", guestConn)
+        log.Println("Get guest connection from pool:", guestConn)
         delete(waitingGuestConnectionPool, sessionId)
         if (guestConn != nil) {
             natgo.ConnectionExchange(conn, guestConn)
@@ -105,26 +104,26 @@ func processMgrChannel(conn net.Conn) {
         buffer := make([]byte, 1)
         _, err := conn.Read(buffer)
         if (err != nil) {
-            fmt.Println("Failed to read from client, ", err)
+            log.Println("Failed to read from client, ", err)
             conn.Close()
             return
         }
         cmd := buffer[0]
         if (cmd == natgo.CMD_HEART_BEAT_REQUEST) {
-            fmt.Println("Get heartbeat request from client")
+            log.Println("Get heartbeat request from client")
             rsp := []byte{natgo.CMD_HEART_BEAT_RESPONSE}
             mgrChannelLock.Lock()
-            fmt.Println("Response heartbeat to client")
+            log.Println("Response heartbeat to client")
             _, err = conn.Write(rsp)
             mgrChannelLock.Unlock()
             if (err != nil) {
-                fmt.Println("Failed to write client")
+                log.Println("Failed to write client")
                 conn.Close()
                 return
             }
         } else if (cmd == natgo.CMD_SERVER_START_SESSION_RESPONSE) {
             //do nothing for now
-            fmt.Println("Get response from client for start session")
+            log.Println("Get response from client for start session")
         }
     }
 }
@@ -136,27 +135,27 @@ func listenForGuests(guestPorts []string) {
 }
 
 func listenForGuest(guestPort string) {
-    fmt.Println("Start Guest thread on port " + guestPort)
+    log.Println("Start Guest thread on port " + guestPort)
     ln, err := net.Listen("tcp", ":" + guestPort)
     if err != nil {
-        fmt.Println(err)
+        log.Println(err)
         panic(err)
     }
-    fmt.Println("Listen on port:" + guestPort, ln.Addr())
+    log.Println("Listen on port:" + guestPort, ln.Addr())
     defer ln.Close()
 
     for {
         guestConn, err := ln.Accept()
         if err != nil {
-            fmt.Println(err)
+            log.Println(err)
             guestConn.Close()
         } else {
-            fmt.Println("Got a connection from guest:", guestConn.RemoteAddr())
+            log.Println("Got a connection from guest:", guestConn.RemoteAddr())
             natConn := natConnectionPool[guestPort]
             if natConn != nil {
                 connectNATAndGuest(natConn, guestConn, guestPort)
             } else {
-                fmt.Println("No client register for this port, close guest.")
+                log.Println("No client register for this port, close guest.")
                 guestConn.Close()
             }
         }
@@ -169,11 +168,11 @@ func connectNATAndGuest(natConn net.Conn, guestConn net.Conn, guestPort string) 
     err := natgo.ServerStartSessionRequest(natConn, sessionId, guestPort)
     mgrChannelLock.Unlock()
     if (err == nil) {
-        fmt.Println("Get correct response, begin start session")
+        log.Println("Get correct response, begin start session")
         waitingGuestConnectionPool[sessionId] = guestConn
         go timeoutForGuestConnection(sessionId)
     } else {
-        fmt.Print("Get invalid response:")
+        log.Print("Get invalid response:")
     }
 }
 
@@ -181,7 +180,7 @@ func timeoutForGuestConnection(sessionId int32) {
     time.Sleep(5 * time.Second)
     conn, ok := waitingGuestConnectionPool[sessionId]
     if (ok) {
-        fmt.Println("Timeout to wait for connection from client, close the guest.")
+        log.Println("Timeout to wait for connection from client, close the guest.")
         conn.Close()
         delete(waitingGuestConnectionPool, sessionId)
     }
